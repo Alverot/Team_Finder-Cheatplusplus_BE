5eb8a20579cd68c757452fe9d6cae22fb0f8deef
Alio <119178113+Alverot@users.noreply.github.com>
2024-03-09 10:55:53 +0200

    Created the project
    Created the project with spring boot and opened it in intellij

---
+++ TeamFinder/src/main/java/com/TeamFinderChPP/TeamFinder/TeamFinderApplication.java
@@ -0,0 +1,13 @@
+package com.TeamFinderChPP.TeamFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFinderApplication.class, args);
+	}
+
+}
